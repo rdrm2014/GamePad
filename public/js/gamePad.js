8402cf75444bbe701bffe8ea1a9a6f7d4bc2ffec
Ricardo Mendes <ricardo.mendes@ua.pt>
2015-05-18 08:58:23 +0100

    Delete Bugs

--- public/js/gamePad.js
+++ public/js/gamePad.js
@@ -13,25 +13,25 @@ function reportOnGamepad() {
     var gp = navigator.getGamepads()[0];
 
     socket.emit('gamepad', {'button1':(gp.buttons[0].pressed) ? true : false});
+    
+    $("#button1").css({'color': (gp.buttons[0].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#button2").css({'color': (gp.buttons[1].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#button3").css({'color': (gp.buttons[2].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#button4").css({'color': (gp.buttons[3].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#buttonL1").css({'color': (gp.buttons[4].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#buttonR1").css({'color': (gp.buttons[5].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#buttonL2").css({'color': (gp.buttons[6].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#buttonR2").css({'color': (gp.buttons[7].pressed) ? '#2989d8' : '#5a5a5a'});
+    $("#buttonSelect").css({'color': (gp.buttons[8].pressed) ? '#2989d8' : '#4d4d4d'});
+    $("#buttonStart").css({'color': (gp.buttons[9].pressed) ? '#2989d8' : '#4d4d4d'});
+    $("#buttonLeftStick").css({'color': (gp.buttons[10].pressed) ? '#2989d8' : '#4d4d4d'});
+    $("#buttonRightStick").css({'color': (gp.buttons[11].pressed) ? '#2989d8' : '#4d4d4d'});
 
-    $("#button1").css({'fill': (gp.buttons[0].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#button2").css({'fill': (gp.buttons[1].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#button3").css({'fill': (gp.buttons[2].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#button4").css({'fill': (gp.buttons[3].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#buttonL1").css({'fill': (gp.buttons[4].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#buttonR1").css({'fill': (gp.buttons[5].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#buttonL2").css({'fill': (gp.buttons[6].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#buttonR2").css({'fill': (gp.buttons[7].pressed) ? '#2989d8' : '#5a5a5a'});
-    $("#buttonSelect").css({'fill': (gp.buttons[8].pressed) ? '#2989d8' : '#4d4d4d'});
-    $("#buttonStart").css({'fill': (gp.buttons[9].pressed) ? '#2989d8' : '#4d4d4d'});
-    $("#buttonLeftStick").css({'fill': (gp.buttons[10].pressed) ? '#2989d8' : '#4d4d4d'});
-    $("#buttonRightStick").css({'fill': (gp.buttons[11].pressed) ? '#2989d8' : '#4d4d4d'});
+    $("#stick2").css({'color': (gp.axes[2]!=0 && gp.axes[3]!=0) ? '#2989d8' : '#4d4d4d'})
+        .text("x1: " + gp.axes[3] + " y1: " + gp.axes[2]);
 
-    $("#stick2").css({'fill': (gp.axes[2]!=0 && gp.axes[3]!=0) ? '#2989d8' : '#4d4d4d'})
-        .text("x: " + gp.axes[3] + " y: " + gp.axes[2]);
-
-    $("#stick3").css({'fill': (gp.axes[4]!=0 && gp.axes[5]!=0) ? '#2989d8' : '#4d4d4d'})
-        .text("x: " + gp.axes[5] + " y: " + gp.axes[4]);
+    $("#stick3").css({'color': (gp.axes[4]!=0 && gp.axes[5]!=0) ? '#2989d8' : '#4d4d4d'})
+        .text("x2: " + gp.axes[4] + " y2: " + gp.axes[5]);    
 }
 
 $(document).ready(function() {
